a1d20f1f1f6e3b47c30c493c7d9bb5823ed97366
samibrahmipod <samibrahmipod@outlook.com>
2021-03-14 18:58:25 -0400

    Add project files.

---
+++ GlobalTicket.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository
+    {
+    }
+}
---
+++ GlobalTicket.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalTicket.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public int TotalOrer { get; set; }
+        public DateTime OrderOlaced { get; set; }
+        public int OrderPaid { get; set; }
+    }
+}
