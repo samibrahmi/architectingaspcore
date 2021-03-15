eb2146389a1cdba215dd6bfc9168f3e1db7e21a7
samibrahmipod <samibrahmipod@outlook.com>
2021-03-14 20:08:29 -0400

    Implementing a Repository interface

--- GlobalTicket.Application/Contracts/Persistence/IAsyncRepository.cs
+++ GlobalTicket.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GlobalTicket.Application.Contracts.Persistence
 {
-    public interface IAsyncRepository
+    public interface IAsyncRepository<T> where T: class
     {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+
     }
 }
